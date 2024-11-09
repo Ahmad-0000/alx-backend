31dcacfdac4d0ec0f54c74714d65d7d61c2627a0
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-09 00:12:55 +0000

    3d task's solution

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+LIFO Caching
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFO Caching class"""
+    def __init__(self):
+        """Initializing an object"""
+        super().__init__()
+        self.caching_stack = []
+
+    def put(self, key, item):
+        """Caching data"""
+        if not key or not item:
+            return
+        if len(self.caching_stack) == BaseCaching.MAX_ITEMS:
+            deleted = self.caching_stack.pop()
+            del self.cach_data[deleted]
+            print(f"DISCARD: {deleted}")
+        self.caching_stack.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Getting an item from a cache"""
+        return self.cache_data.get(key)
