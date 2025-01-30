d8e5bb643bbe18d828f8348ecd1569b32e7e9ee5
Ahmad <ahmad.new.m.v@gmail.com>
2025-01-30 19:18:37 +0000

    A new solution

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,28 @@
+import { createQueue } from 'kue';
+
+function createPushNotificationJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+  for (const jobData of jobs) {
+    const job = queue.create('8-job', jobData);
+    job.on('enqueue', () => {
+      console.log('Notification job created: ', job.id);
+    });
+
+   job.on('completed', () => {
+     console.log(`Notification job ${job.id} completed`)
+   });
+
+   job.on('failed', (error) => {
+     console.log(`Notification job ${job.id} failed: ${error}`);
+   });
+
+   job.on('progress', (progress) => {
+     console.log(`Notification job ${job.id} ${progress}% complete`);
+   });
+   job.save();
+  }
+}
+
+export default createPushNotificationJobs;
